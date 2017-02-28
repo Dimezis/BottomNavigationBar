084cdef0946d146b3b4282485c5f13b518ad018a
Dimezis <Dmitry.Savyuk@gmail.com>
2017-02-28 22:00:09 +0100

    Update example project

--- app/src/main/java/com/eightbitlab/test/MainActivity.java
+++ app/src/main/java/com/eightbitlab/test/MainActivity.java
@@ -1,7 +1,9 @@
 package com.eightbitlab.test;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.eightbitlab.bottomnavigationbar.BottomBarItem;
 import com.eightbitlab.bottomnavigationbar.BottomNavigationBar;
@@ -12,15 +14,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = ((TextView) findViewById(R.id.textView));
+        showContent(0, textView);
+        setupBottomBar(textView);
+    }
+
+    private void setupBottomBar(@NonNull final TextView textView) {
         BottomNavigationBar bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_bar);
 
-        BottomBarItem item1 = BottomBarItem.builder().iconId(R.drawable.test_icon).build();
-        BottomBarItem item2 = BottomBarItem.builder().iconId(R.drawable.test_icon).title(R.string.app_name).build();
+        BottomBarItem item = BottomBarItem.builder()
+                .iconId(R.drawable.test_icon)
+                .build();
 
         bottomNavigationBar
-                .addTab(item1)
-                .addTab(item2)
-                .addTab(item2)
-                .addTab(item2);
+                .addTab(item)
+                .addTab(item)
+                .addTab(item)
+                .addTab(item);
+
+        bottomNavigationBar.setOnTabClickListener(new BottomNavigationBar.OnTabClickListener() {
+            @Override
+            public void onClick(int position) {
+                showContent(position, textView);
+            }
+        });
+    }
+
+    void showContent(int position, @NonNull TextView textView) {
+        textView.setText("Tab " + position + " selected");
     }
 }
