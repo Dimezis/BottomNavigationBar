947996874af077722be750f88f5c59e97b301a54
saviukd <Dmytro.Saviuk@ig.com>
2017-03-18 21:08:09 +0100

    Add navigation bar padding only if device has software navbar

--- app/src/main/java/com/eightbitlab/test/MainActivity.java
+++ app/src/main/java/com/eightbitlab/test/MainActivity.java
@@ -1,11 +1,18 @@
 package com.eightbitlab.test;
 
+import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
+import android.view.ViewConfiguration;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.eightbitlab.bottomnavigationbar.BottomBarItem;
@@ -42,11 +49,15 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //only for translucent system navbar
-        if (atLeastKitkat() && isInPortrait()) {
+        if (shouldAddNavigationBarPadding()) {
             //if your bottom bar has fixed height
             //you'll need to increase its height as well
             bottomNavigationBar.setPadding(0, 0, 0, getSystemNavigationBarHeight());
         }
+    }
+
+    private boolean shouldAddNavigationBarPadding() {
+        return atLeastKitkat() && isInPortrait() && hasSystemNavigationBar();
     }
 
     private boolean isInPortrait() {
@@ -64,13 +75,37 @@ public class MainActivity extends AppCompatActivity {
     private int getSystemNavigationBarHeight() {
         Resources res = getResources();
 
-        int identifier = res.getIdentifier("navigation_bar_height", "dimen", "android");
+        int id = res.getIdentifier("navigation_bar_height", "dimen", "android");
         int height = 0;
 
-        if (identifier > 0) {
-            height = res.getDimensionPixelSize(identifier);
+        if (id > 0) {
+            height = res.getDimensionPixelSize(id);
         }
 
         return height;
+    }
+
+    private boolean hasSystemNavigationBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            Display d = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+
+            DisplayMetrics realDisplayMetrics = new DisplayMetrics();
+            d.getRealMetrics(realDisplayMetrics);
+
+            int realHeight = realDisplayMetrics.heightPixels;
+            int realWidth = realDisplayMetrics.widthPixels;
+
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            d.getMetrics(displayMetrics);
+
+            int displayHeight = displayMetrics.heightPixels;
+            int displayWidth = displayMetrics.widthPixels;
+
+            return (realWidth - displayWidth) > 0 || (realHeight - displayHeight) > 0;
+        } else {
+            boolean hasMenuKey = ViewConfiguration.get(this).hasPermanentMenuKey();
+            boolean hasBackKey = KeyCharacterMap.deviceHasKey(KeyEvent.KEYCODE_BACK);
+            return !hasMenuKey && !hasBackKey;
+        }
     }
 }
